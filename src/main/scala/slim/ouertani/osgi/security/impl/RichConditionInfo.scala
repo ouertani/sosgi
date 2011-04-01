0187d4fc5ca12169564d2c9ce2e49455f795dad8
slim <ouertani@gmail.com>
2011-04-01 14:56:12 +0200

    override to string

--- src/main/scala/slim/ouertani/osgi/security/impl/RichConditionInfo.scala
+++ src/main/scala/slim/ouertani/osgi/security/impl/RichConditionInfo.scala
@@ -1,12 +1,9 @@
-
-
 package slim.ouertani.osgi.security.impl
-
-
-
 
 
 case class RichConditionInfo( tipe:java.lang.String,
                          args:List[String]){
 
+
+  override def toString = "\n " + tipe + args.mkString(" ")
 }
